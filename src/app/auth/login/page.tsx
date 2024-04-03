4319dee72788e38e6a73359059773be3b7110cbc
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-04 02:30:55 +0300

    fdgd

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,11 +3,7 @@ import Login from "../../../../components/views/login/page";
 const LoginPage = () => {
     return (
         <>
-<<<<<<< HEAD
-            <h6>masteryyyyfffffffffffyyyffffffffyytest</h6>
-=======
             <h6>masteryyyyfffffffffffddddyyyyytest</h6>
->>>>>>> a599b4f0f28679a077a64ccc7c17bf934f6f3c5e
             <Login />
             <footer className="footer">
                 <div className="copyright-text">
