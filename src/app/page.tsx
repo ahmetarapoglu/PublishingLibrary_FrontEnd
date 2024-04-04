b266f322f94a685aac0700ecab9238381842a119
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-04 20:25:54 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ import { path } from "../../service/path";
 export default function Home() {
   const router = useRouter();
   const router = useRouter();
-  const router = useRouter();
-  const router = useRouter();
-  const router = useRouter();
+
 
   router.push(path.login)
   return
