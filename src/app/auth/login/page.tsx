ae92afb61e3ca02d8febe4c6afa1702aacfa44c2
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-04 20:13:35 +0300

    ssss

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import Login from "../../../../components/views/login/page";
 const LoginPage = () => {
     return (
         <>
-            <h6>test</h6>
+            <h6>testhhhhhhhhh</h6>
             <Login />
             <footer className="footer">
                 <div className="copyright-text">
