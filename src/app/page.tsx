21830c0eab146c95d45ebc77bfb2bb4d97c926db
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-04 20:26:37 +0300

    asda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import { path } from "../../service/path";
 
 export default function Home() {
   const router = useRouter();
-<<<<<<< HEAD
-=======
-  const router = useRouter();
->>>>>>> b266f322f94a685aac0700ecab9238381842a119
 
 
   router.push(path.login)
