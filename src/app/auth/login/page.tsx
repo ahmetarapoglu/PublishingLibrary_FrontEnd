4d1687b5e25d9bac38a2984b1f7aaeac81f6ec0f
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-20 10:16:19 +0300

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,9 +3,9 @@ import Login from "../../../../components/views/login/page";
 const LoginPage = () => {
     return (
         <>
-            <h6>master1</h6>
-            <h6>master2</h6>
             <h6>master3</h6>
+            <h6>master2</h6>
+            <h6>master1</h6>
             <Login/>
             <footer className="footer">
                 <div className="copyright-text">
