f410eb20595c9d3f7f70306da378d31024df449e
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-20 10:46:19 +0300

    aaaaaa

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,11 +3,6 @@ import Login from "../../../../components/views/login/page";
 const LoginPage = () => {
     return (
         <>
-            <h6>master6</h6>
-            <h6>master5</h6>
-            <h6>master4</h6>
-            <h6>master3</h6>
-            <h6>master2</h6>
             <h6>master1</h6>
             <Login/>
             <footer className="footer">
