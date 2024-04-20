834e05c3d9c2b8ba1c15cb5112151b7f6b6a71ac
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-20 12:15:24 +0300

    Update test.tsx

--- src/app/auth/login/test.tsx
+++ src/app/auth/login/test.tsx
@@ -1,1 +1,11 @@
+import Login from "../../../../components/views/login/page";
 
+const Test = () => {
+    return (
+        <>
+            <h6>Test</h6>
+        </>
+    );
+};
+
+export default Test;
