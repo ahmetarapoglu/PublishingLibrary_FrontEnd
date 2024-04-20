52d5aed6564ac05267e5327eb917c2328180f002
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-04-20 10:36:20 +0300

    sasasasas

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,6 +3,7 @@ import Login from "../../../../components/views/login/page";
 const LoginPage = () => {
     return (
         <>
+            <h6>master6</h6>
             <h6>master5</h6>
             <h6>master4</h6>
             <h6>master3</h6>
