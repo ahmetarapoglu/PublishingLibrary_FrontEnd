bf23fb496c867fb2822bd6ba1d72985b4d6beccf
AHMET ARAPOĞLU <45310027+ahmetarapoglu@users.noreply.github.com>
2024-10-13 23:09:43 +0300

    aaaaaa

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,15 +3,12 @@ import Login from "../../../../components/views/login/page";
 const LoginPage = () => {
     return (
         <>
-            {/* <h6>master</h6>
-            <h1>Updated</h1> */}
-
-            {/*<Login/>*/}
-            {/*<footer className="footer">*/}
-            {/*    <div className="copyright-text">*/}
-            {/*        BookShop ©2023 Created By AR7B.*/}
-            {/*    </div>*/}
-            {/*</footer>*/}
+            <Login />
+            <footer className="footer">
+                <div className="copyright-text">
+                    sfsdfsfdsdf
+                </div>
+            </footer>
         </>
     );
 };
